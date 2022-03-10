d618296263e33d9e8eadab101f181ca365b6c497
Michael Hatch <michaelhatch@me.com>
2022-03-09 19:48:42 -0800

    update

--- app.js
+++ app.js
@@ -11,15 +11,40 @@ if (localStorage.getItem("data")) {
 } else {
   setLocalData();
 }
+function displayBoard(state) {
+  // TODO
+}
 function updateBoard(player, pocket) {
-  console.log(player, pocket);
   const currState = data[data.length - 1];
   const newState = [...currState];
   const chosenIndex = player === 1 ? pocket - 1 : 6 + pocket;
   let beads = currState[chosenIndex];
   newState[chosenIndex] = 0;
-  // TODO
-  // data.push( new state );
+  let skipped = 0;
+  Array(beads)
+    .fill(1)
+    .forEach((_, bead) => {
+      const dataIndex = (chosenIndex + bead + skipped) % 14;
+      if (
+        (player === 1 && dataIndex === 13) ||
+        (player === 2 && dataIndex === 6)
+      ) {
+        skipped++;
+      } else {
+        newState[dataIndex]++;
+      }
+    });
+  if (skipped) {
+    const ogTotes = currState[chosenIndex];
+    Array(skipped)
+      .fill(1)
+      .forEach((_, skip) => {
+        newState[(chosenIndex + skip + ogTotes) % 14]++;
+      });
+  }
+  data.push(newState);
+  setLocalData();
+  displayBoard(newState);
 }
 [...document.getElementsByClassName("little-pocket")].forEach((el, index) => {
   const player = index < 6 ? 2 : 1;
@@ -30,11 +55,15 @@ function updateBoard(player, pocket) {
 });
 document.getElementById("btn-undo").addEventListener("click", () => {
   if (confirm("Undo last move?")) {
-    console.log("TODO: Undo last move.");
+    data.pop();
+    setLocalData();
+    displayBoard(data[data.length - 1]);
   }
 });
 document.getElementById("btn-reset").addEventListener("click", () => {
   if (confirm("Reset the board?")) {
-    console.log("TODO: Reset the board.");
+    data = [[4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]];
+    setLocalData();
+    displayBoard(data[0]);
   }
 });
